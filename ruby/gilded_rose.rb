54eac30d99a9d404aee4e7a0a52d53354e0e0fbd
deepak verma <deepak.v@goldeneagle.ai>
2024-01-23 15:09:27 +0530

    Conjoured item

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,57 +1,59 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_item(item)
     end
   end
+
+  private
+
+  def update_item(item)
+    if item.name == "Aged Brie"
+      update_aged_brie(item)
+    elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+      update_backstage_passes(item)
+    elsif item.name == "Sulfuras, Hand of Ragnaros"
+      # Sulfuras is a legendary item and does not change
+    elsif item.name.include?("Conjured")
+      update_conjured(item)
+    else
+      update_normal_item(item)
+    end
+  end
+
+  def update_aged_brie(item)
+    item.quality = [item.quality + 1, 50].min
+    item.sell_in -= 1
+  end
+
+  def update_backstage_passes(item)
+    item.sell_in -= 1
+    if item.sell_in >= 10
+      item.quality = [item.quality + 1, 50].min
+    elsif item.sell_in >= 5
+      item.quality = [item.quality + 2, 50].min
+    elsif item.sell_in >= 0
+      item.quality = [item.quality + 3, 50].min
+    else
+      item.quality = 0
+    end
+  end
+
+  def update_conjured(item)
+    item.sell_in -= 1
+    item.quality = [item.quality - 2, 0].max
+  end
+
+  def update_normal_item(item)
+    item.sell_in -= 1
+    item.quality = [item.quality - (item.sell_in >= 0 ? 1 : 2), 0].max
+  end
 end
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -6,6 +6,29 @@ describe GildedRose do
   it "does not change the name" do
     items = [Item.new("foo", 0, 0)]
     GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "fixme"
+    expect(items[0].name).to eq "foo"
+  end
+end
+
+describe '#update_quality' do
+  context 'when updating quality for a Conjured item' do
+    it 'decreases quality by 2 and decreases sell-in by 1' do
+      conjured_item = Item.new("Conjured Mana Cake", 5, 20)
+      gilded_rose = GildedRose.new([conjured_item])
+
+      gilded_rose.update_quality()
+
+       expect(conjured_item.sell_in).to eq(4)
+       expect(conjured_item.quality).to eq(18)
+    end
+  
+    it 'does not decrease quality below 0' do
+      conjured_item = Item.new("Conjured Mana Cake", 0, 1)
+      gilded_rose = GildedRose.new([conjured_item])
+
+      gilded_rose.update_quality()
+
+      expect(conjured_item.quality).to eq(0)
+    end
   end
 end
